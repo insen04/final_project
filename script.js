0bc109dcba863d10a06c9cc0a1518ba33f39b74f
insen04 <71251635+insen04@users.noreply.github.com>
2021-03-26 12:40:37 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("navigation").classList.toggle("change");
+    document.getElementById("menu_circle").classList.toggle("change_circle");
+}
